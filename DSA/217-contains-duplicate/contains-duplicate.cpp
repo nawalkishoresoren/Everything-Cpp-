737149bd8a8cc4e9a921a8667143589eb14f9d88
nawalkishoresoren <nawalkishoresoren@gmail.com>
2026-07-15 17:02:43 +0530

    Time: 26 ms (85.55%) | Memory: 82.6 MB (84.44%) - LeetSync

--- DSA/217-contains-duplicate/contains-duplicate.cpp
+++ DSA/217-contains-duplicate/contains-duplicate.cpp
@@ -2,7 +2,16 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) 
     {
-        set<int>st(nums.begin(),nums.end());
-        return !(nums.size()==st.size());
+        if(nums.size()==1)
+            return false;
+
+        sort(nums.begin(),nums.end());
+
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i] == nums[i+1])
+                return true;
+        }
+        return false;
     }
 };
