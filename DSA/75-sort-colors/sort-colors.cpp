7b329b55cd72f198280c778adea14af7b6f1f97d
nawalkishoresoren <nawalkishoresoren@gmail.com>
2026-07-17 21:29:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (11.68%) - LeetSync

--- DSA/75-sort-colors/sort-colors.cpp
+++ DSA/75-sort-colors/sort-colors.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) 
     {
-        int left = 0, mid = 0, right = nums.size()-1;
+        int left = 0,mid = 0,right = nums.size()-1;
 
         while(mid<=right)
         {
