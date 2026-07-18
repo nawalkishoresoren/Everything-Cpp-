b5353f2e31774e567abcff03cafe81d9ac3430fb
nawalkishoresoren <nawalkishoresoren@gmail.com>
2026-07-18 13:22:18 +0530

    Time: 27 ms (39.96%) | Memory: 88.3 MB (73.26%) - LeetSync

--- DSA/525-contiguous-array/contiguous-array.cpp
+++ DSA/525-contiguous-array/contiguous-array.cpp
@@ -3,21 +3,17 @@ public:
     int findMaxLength(vector<int>& nums) 
     {
         unordered_map<int,int>umap;
+        umap[0] = -1;
         int sum = 0, max_arr = 0;
         for(int i=0;i<nums.size();i++)
         {
             if(nums[i]==1)
             {
                 sum += 1;
-                if(sum==0)
-                    max_arr = max(max_arr,i+1);
-
             }
             else
             {
                 sum -= 1;
-                if(sum==0)
-                    max_arr = max(max_arr,i+1);
             }
 
             if(umap.find(sum) == umap.end())
