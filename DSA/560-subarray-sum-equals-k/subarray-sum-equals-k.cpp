4a0285e8db8ed8a2790572b9e94af7a8f1ed47cc
nawalkishoresoren <nawalkishoresoren@gmail.com>
2026-07-20 16:42:40 +0530

    Time: 63 ms (32.33%) | Memory: 51 MB (14.76%) - LeetSync

--- DSA/560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ DSA/560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -11,16 +11,10 @@ public:
         {
             prefix_sum += nums[i];
             
-            if(umap.find(prefix_sum - k) == umap.end())
-            {
-                umap[prefix_sum]++;
-            }
-            else
-            {
-                result += umap[prefix_sum - k] * 1;
-                umap[prefix_sum]++;
-            }
-
+            
+            result += umap[prefix_sum - k];
+            umap[prefix_sum]++;
+            
         }
         return result;
     }
