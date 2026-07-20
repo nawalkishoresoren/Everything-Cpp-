77a356a59057dbe6bfd26d4725e54e5386c0ff3b
nawalkishoresoren <nawalkishoresoren@gmail.com>
2026-07-20 16:23:35 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (86.43%) - LeetSync

--- DSA/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ DSA/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -3,24 +3,21 @@ public:
     int lengthOfLongestSubstring(string s) 
     {
         int char_freq[256] = {0};
-        int left = 0,right = 0;
-        int max_length = 0;
+        int left = 0, right = 0, max_length = 0;
 
         while(right<s.size())
         {
             char_freq[s[right]]++;
-            
-            if(char_freq[s[right]] > 1)
+
+            while(char_freq[s[right]]>1)
             {
-                while(char_freq[s[right]]>1)
-                {
-                    char_freq[s[left]]--;
-                    left++;
-                }
+                char_freq[s[left]]--;
+                left++;
             }
             max_length = max(max_length,right-left+1);
             right++;
         }
         return max_length;
+        
     }
 };
