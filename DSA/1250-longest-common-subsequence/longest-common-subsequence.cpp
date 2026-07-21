ed6049ceff928e8eea981a12852184ae60c7b12a
nawalkishoresoren <nawalkishoresoren@gmail.com>
2026-07-21 11:44:15 +0530

    Time: 31 ms (38.30%) | Memory: 13 MB (84.71%) - LeetSync

--- DSA/1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ DSA/1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -3,19 +3,18 @@ public:
     int dp[1001][1001];
     int solve(int i, string &s1, int j, string &s2)
     {
-        if(i>=s1.size() || j>=s2.size())
+        if(i>=s1.size() || j>= s2.size())
             return 0;
 
         if(dp[i][j] != -1)
             return dp[i][j];
 
         if(s1[i] == s2[j])
-        {
             return dp[i][j] = 1 + solve(i+1,s1,j+1,s2);
-        }
-        return dp[i][j] = max(solve(i+1,s1,j,s2),solve(i,s1,j+1,s2));
-    }
 
+        return dp[i][j] = max(solve(i+1,s1,j,s2),solve(i,s1,j+1,s2));
+
+    }
     int longestCommonSubsequence(string text1, string text2) 
     {
         memset(dp,-1,sizeof(dp));
