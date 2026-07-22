27068b62e75609c1e08a28a317b9d5def4fa1e8b
nawalkishoresoren <nawalkishoresoren@gmail.com>
2026-07-22 19:28:32 +0530

    Time: 4 ms (37.68%) | Memory: 14.4 MB (33.34%) - LeetSync

---
+++ DSA/39-combination-sum/combination-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void solve(int idx,vector<int>& candidates, vector<int>& temp, int target)
+    {
+        if(idx==candidates.size())
+        {
+            if(target == 0)
+            {
+                ans.push_back(temp);
+            }
+            return;
+        }
+        if(candidates[idx]<=target)
+        {
+            temp.push_back(candidates[idx]);
+            solve(idx,candidates,temp,target - candidates[idx]);
+            temp.pop_back();
+        }
+        solve(idx+1,candidates,temp,target);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
+    {
+        vector<int>temp;
+        solve(0,candidates,temp,target);
+        return ans;
+    }
+};
